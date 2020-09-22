91a47a7c3c1a02a71f33a430d7eb9421df8c9d98
KaroRonty <karo.ronty@gmail.com>
2020-09-22 19:21:59 +0200

    Removed hardcoding and added more models

--- modeling.R
+++ modeling.R
@@ -3,10 +3,15 @@ library(dplyr)
 library(tidyr)
 library(janitor)
 library(ggplot2)
+library(lubridate)
 
 library(fable)
 library(tsibble)
 library(feasts)
+
+# Define date to do the training/test set split
+split_date <- "1995-01-01"
+leakage_end_date <- as.Date(split_date) + years(10)
 
 capes_wide <- read_excel("Data/cape.xls")
 
@@ -29,20 +34,40 @@ to_model <- capes_long %>%
   as_tsibble(key = "country", index = "date")
 
 training <- to_model %>% 
-  filter(date < yearmonth("1995-01-01"))
+  filter(date < yearmonth(split_date))
 
 test <- to_model %>% 
-  filter(date >= yearmonth("2005-01-01"))
+  filter(date >= yearmonth(leakage_end_date))
 
 models_ts <- training %>% 
-  model(ARIMA = ARIMA(cagr_10_year ~ cape))
+  model(ARIMA = ARIMA(cagr_10_year ~ cape),
+        MEAN = MEAN(cagr_10_year),
+        ETS = ETS(cagr_10_year),
+        RW = RW(cagr_10_year),
+        NAIVE = NAIVE(cagr_10_year),
+        SNAIVE = SNAIVE(cagr_10_year),
+        NNETAR = NNETAR(cagr_10_year),
+        AR = AR(cagr_10_year),
+        VAR = VAR(cagr_10_year)) %>% 
+  mutate(COMBINATION = (ARIMA + SNAIVE + VAR + NAIVE + RW) / 5,
+         ARIMA_SNAIVE = (ARIMA + SNAIVE) / 2)
 
 # Training set accuracy
 models_ts %>% accuracy()
 
+models_ts %>% print(n = 50)
+
+fcast <- models_ts %>% 
+  forecast(test)
+
 # Remove leakage
 fcast_no_leakage <- fcast %>% 
-  filter(date > yearmonth("2005-01-01"))
+  filter(date > yearmonth(leakage_end_date))#,
+         # .model == "ARIMA")
+
+# mean_fcast_no_leakage <- fcast %>% 
+#   filter(date > yearmonth("2005-01-01"),
+#          .model == "MEAN(cagr_10_year)")
 
 
 acc_no_leakage <- fcast_no_leakage %>% accuracy(bind_rows(training, test)) %>% 
@@ -50,3 +75,36 @@ acc_no_leakage <- fcast_no_leakage %>% accuracy(bind_rows(training, test)) %>%
   filter(!is.na(MAE)) %>% 
   arrange(MAE) %>% 
   print(n = 100)
+
+mean_fcast_acc <- acc_no_leakage %>% arrange(MAE)
+
+fcast %>%
+  # filter(country == "AUSTRALIA") %>% 
+  autoplot(level = NULL) +
+  autolayer(test, # %>% 
+              # filter(country == "AUSTRALIA"), 
+            cagr_10_year,
+            color = "black") +
+  facet_wrap(~ country) + 
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+arima_acc <- mean_fcast_acc %>% 
+  filter(.model == "ARIMA")
+
+non_arima_acc <- mean_fcast_acc %>% 
+  filter(.model == "MEAN") %>% 
+  mutate(MAE_mean = MAE) %>% 
+  select(country, MAE_mean)
+
+arima_acc %>% 
+  full_join(non_arima_acc) %>% 
+  mutate(MAE_diff = MAE - MAE_mean) %>% 
+  arrange(MAE_diff) %>% 
+  print(n = 50)
+
+# Average accuracy of every model
+acc_no_leakage %>% 
+  group_by(.model) %>% 
+  summarise(MAE = mean(MAE, na.rm = TRUE)) %>% 
+  arrange(MAE)
