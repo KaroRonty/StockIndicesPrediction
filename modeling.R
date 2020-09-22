c17933afe7b5b7aafa416fe5c992d3a0d34b7bb1
KaroRonty <karo.ronty@gmail.com>
2020-09-22 22:36:44 +0200

    Added the 10-year interest rate as a predictor and unemployment rate data

--- modeling.R
+++ modeling.R
@@ -13,17 +13,21 @@ library(feasts)
 split_date <- "1995-01-01"
 leakage_end_date <- as.Date(split_date) + years(10)
 
+# CAPEs -------------------------------------------------------------------
 capes_wide <- read_excel("Data/cape.xls")
 
 capes_long <- capes_wide %>% 
-  pivot_longer(AUSTRALIA:USA, names_to = "country", values_to = "cape") %>% 
+  pivot_longer(AUSTRALIA:USA, names_to = "country", values_to = "cape") %>%  # FIXME
   mutate(cape = ifelse(cape == 0, NA, cape),
          date = yearmonth(date))
 
-prices_local_wide <- read_excel("Data/loc.xls")
+# Prices ------------------------------------------------------------------
+prices_local_wide <- read_excel("Data/loc2.xlsx")
 
 prices_local_long <- prices_local_wide %>%
-  pivot_longer(AUSTRALIA:ZIMBABWE, names_to = "country", values_to = "price") %>% 
+  pivot_longer(AUSTRALIA:UNITED_ARAB_EMIRATES, # FIXME
+               names_to = "country",
+               values_to = "price") %>% 
   group_by(country) %>% 
   mutate(price = ifelse(price == 0, NA, price),
          date = yearmonth(date),
@@ -33,6 +37,28 @@ to_model <- capes_long %>%
   inner_join(prices_local_long) %>% 
   as_tsibble(key = "country", index = "date")
 
+
+# Macro -------------------------------------------------------------------
+unemployment_wide <- read_excel("Data/macro_m.xlsx", sheet = "unr_sa") # unr_na
+
+unemployment_long <- unemployment_wide %>% 
+  pivot_longer(-date, names_to = "country", values_to = "unemployment") %>%  # FIXME
+  mutate(date = yearmonth(date))
+
+rate_10_year_wide <- read_excel("Data/macro_m.xlsx", sheet = "ltir")
+
+rate_10_year_long <- rate_10_year_wide %>% 
+  pivot_longer(-date, names_to = "country", values_to = "rate_10_year") %>%  # FIXME
+  mutate(date = yearmonth(date))
+
+to_model <- capes_long %>% 
+  inner_join(prices_local_long) %>% 
+  # inner_join(unemployment_long) %>%
+  inner_join(rate_10_year_long)  %>% 
+  na.omit() %>% # FIXME
+  as_tsibble(key = "country", index = "date")
+
+# Models ------------------------------------------------------------------
 training <- to_model %>% 
   filter(date < yearmonth(split_date))
 
@@ -40,22 +66,22 @@ test <- to_model %>%
   filter(date >= yearmonth(leakage_end_date))
 
 models_ts <- training %>% 
-  model(ARIMA = ARIMA(cagr_10_year ~ cape),
-        MEAN = MEAN(cagr_10_year),
-        ETS = ETS(cagr_10_year),
-        RW = RW(cagr_10_year),
-        NAIVE = NAIVE(cagr_10_year),
-        SNAIVE = SNAIVE(cagr_10_year),
-        NNETAR = NNETAR(cagr_10_year),
-        AR = AR(cagr_10_year),
-        VAR = VAR(cagr_10_year)) %>% 
-  mutate(COMBINATION = (ARIMA + SNAIVE + VAR + NAIVE + RW) / 5,
-         ARIMA_SNAIVE = (ARIMA + SNAIVE) / 2)
+  model(ARIMA = ARIMA(cagr_10_year ~ cape + rate_10_year),
+        MEAN = MEAN(cagr_10_year))#,
+  #       ETS = ETS(cagr_10_year),
+  #       RW = RW(cagr_10_year),
+  #       NAIVE = NAIVE(cagr_10_year),
+  #       SNAIVE = SNAIVE(cagr_10_year),
+  #       NNETAR = NNETAR(cagr_10_year),
+  #       AR = AR(cagr_10_year),
+  #       VAR = VAR(cagr_10_year)) %>% 
+  # mutate(COMBINATION = (ARIMA + SNAIVE + VAR + NAIVE + RW) / 5,
+  #        ARIMA_SNAIVE = (ARIMA + SNAIVE) / 2)
 
-# Training set accuracy
-models_ts %>% accuracy()
-
-models_ts %>% print(n = 50)
+# # Training set accuracy
+# models_ts %>% accuracy()
+# 
+# models_ts %>% print(n = 50)
 
 fcast <- models_ts %>% 
   forecast(test)
@@ -71,14 +97,30 @@ fcast_no_leakage <- fcast %>%
 
 
 acc_no_leakage <- fcast_no_leakage %>% accuracy(bind_rows(training, test)) %>% 
-  select(.model, country, .type, RMSE, MAE, MASE) %>% 
+  select(.model, country, .type, RMSE, MAE, MAPE) %>% 
   filter(!is.na(MAE)) %>% 
   arrange(MAE) %>% 
   print(n = 100)
 
-mean_fcast_acc <- acc_no_leakage %>% arrange(MAE)
+# TODO
+# Accuracy of the model with additional features
+acc_no_leakage %>% 
+  filter(.model == "ARIMA") %>% 
+  inner_join(acc_no_leakage_arima %>% select(country)) %>% 
+  pull(MAE) %>% mean
+
+# Accuracy of the comparable ARIMA models
+acc_no_leakage_arima %>% 
+  inner_join(acc_no_leakage %>% 
+               filter(.model == "ARIMA") %>% 
+               select(country)) %>% 
+  pull(MAE) %>% 
+  mean(na.rm = TRUE)
+
+fcast_acc <- acc_no_leakage %>% arrange(MAE)
 
 fcast %>%
+  filter(.model == "ARIMA") %>% 
   # filter(country == "AUSTRALIA") %>% 
   autoplot(level = NULL) +
   autolayer(test, # %>% 
@@ -89,19 +131,23 @@ fcast %>%
   theme_minimal() +
   theme(legend.position = "bottom")
 
-arima_acc <- mean_fcast_acc %>% 
+arima_acc <- fcast_acc %>% 
   filter(.model == "ARIMA")
 
 non_arima_acc <- mean_fcast_acc %>% 
   filter(.model == "MEAN") %>% 
-  mutate(MAE_mean = MAE) %>% 
-  select(country, MAE_mean)
+  mutate(MAE_mean = MAE,
+         MAPE_mean = MAPE) %>% 
+  select(country, MAE_mean, MAPE_mean)
 
 arima_acc %>% 
   full_join(non_arima_acc) %>% 
-  mutate(MAE_diff = MAE - MAE_mean) %>% 
-  arrange(MAE_diff) %>% 
-  print(n = 50)
+  mutate(MAE_diff = MAE - MAE_mean,
+         MAPE_diff = MAPE - MAPE_mean,
+         MAPE_div = MAPE_mean / MAPE) %>% 
+  arrange(-MAPE_div) %>% 
+  print(n = 50) #%>%
+   # pull(MAPE_div) %>% mean(na.rm = TRUE)
 
 # Average accuracy of every model
 acc_no_leakage %>% 
