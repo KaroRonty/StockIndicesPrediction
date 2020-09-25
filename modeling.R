13049c4493330dbe9d8e993d64c805595d6214d7
KaroRonty <karo.ronty@gmail.com>
2020-09-25 21:25:45 +0200

    Tidying up the code, adding comments

--- modeling.R
+++ modeling.R
@@ -1,12 +1,11 @@
-library(readxl)
 library(dplyr)
 library(tidyr)
-library(ggplot2)
-library(lubridate)
 library(purrr)
-
 library(fable)
+library(readxl)
+library(ggplot2)
 library(tsibble)
+library(lubridate)
 
 # How many years ahead to calculate CAGRs for
 lead_years <- 1:10
@@ -18,16 +17,31 @@ leakage_end_date <- as.Date(split_date) + years(10)
 # For plotting
 fcast_start_date <- as.Date("1980-01-01")
 
+# Choose model to compare forecasts with
+model_to_compare <- "MEAN"
+
 # CAPEs -------------------------------------------------------------------
 capes_wide <- read_excel("Data/cape.xls")
 
+# Pivot into long format and replace missing values with NAs
 capes_long <- capes_wide %>% 
-  pivot_longer(AUSTRALIA:USA, names_to = "country", values_to = "cape") %>%  # FIXME
+  pivot_longer(AUSTRALIA:USA,
+               names_to = "country",
+               values_to = "cape") %>%
   mutate(cape = ifelse(cape == 0, NA, cape),
          date = yearmonth(date))
 
 # Prices ------------------------------------------------------------------
 prices_local_wide <- read_excel("Data/loc2.xlsx")
+
+# Pivot into long format and replace missing values with NAs
+prices_local_long <- prices_local_wide %>%
+  pivot_longer(-date,
+               names_to = "country",
+               values_to = "price") %>% 
+  group_by(country) %>% 
+  mutate(price = ifelse(price == 0, NA, price),
+         date = yearmonth(date))
 
 # Function for making leaded columns for CAGR
 add_cagr_columns <- function(df, lead){
@@ -37,41 +51,42 @@ add_cagr_columns <- function(df, lead){
     mutate(!!col_name := (lead(price, 12 * lead) / price)^(1 / lead))
 }
 
-prices_local_long <- prices_local_wide %>%
-  pivot_longer(AUSTRALIA:UNITED_ARAB_EMIRATES, # FIXME
-               names_to = "country",
-               values_to = "price") %>% 
-  group_by(country) %>% 
-  mutate(price = ifelse(price == 0, NA, price),
-         date = yearmonth(date))
-
 # Computed separately due to lack of visibility inside a nested function
-prices_local_long <- suppressMessages(map(lead_years,
-                         ~add_cagr_columns(prices_local_long, .x)) %>% 
-  reduce(inner_join))
+prices_local_long <- suppressMessages(
+  map(lead_years,
+      ~add_cagr_columns(prices_local_long, .x)) %>% 
+    reduce(inner_join))
 
 # Macro -------------------------------------------------------------------
 unemployment_wide <- read_excel("Data/macro_m.xlsx", sheet = "unr_sa") # unr_na
 
 unemployment_long <- unemployment_wide %>% 
-  pivot_longer(-date, names_to = "country", values_to = "unemployment") %>%  # FIXME
+  pivot_longer(-date, 
+               names_to = "country", 
+               values_to = "unemployment") %>% 
   mutate(date = yearmonth(date))
 
 rate_10_year_wide <- read_excel("Data/macro_m.xlsx", sheet = "ltir")
 
 rate_10_year_long <- rate_10_year_wide %>% 
-  pivot_longer(-date, names_to = "country", values_to = "rate_10_year") %>%  # FIXME
+  pivot_longer(-date, 
+               names_to = "country", 
+               values_to = "rate_10_year") %>% 
   mutate(date = yearmonth(date))
 
+# Models ------------------------------------------------------------------
+# Join variables
 # FIXME
 to_model <- capes_long %>% 
-  inner_join(prices_local_long) %>% 
+  full_join(prices_local_long %>% 
+              select(date, country, cagr_10_year)) %>% 
   # inner_join(unemployment_long) %>%
-  inner_join(rate_10_year_long) %>% 
+  full_join(rate_10_year_long) %>% 
+  select(date, country, cagr_10_year, cape, rate_10_year) %>% 
   na.omit() %>% # FIXME
   as_tsibble(key = "country", index = "date")
 
-# Models ------------------------------------------------------------------
+# Split into different sets
 training <- to_model %>% 
   filter(date < yearmonth(split_date))
 
@@ -82,38 +97,39 @@ leakage_set <- to_model %>%
 test <- to_model %>% 
   filter(date >= yearmonth(leakage_end_date))
 
+# Train different time series models
 models_ts <- training %>% 
   model(ARIMA = ARIMA(cagr_10_year ~ cape + rate_10_year),
         MEAN = MEAN(cagr_10_year),
         NAIVE = NAIVE(cagr_10_year))#,
-  #       ETS = ETS(cagr_10_year),
-  #       RW = RW(cagr_10_year),
-  #       NAIVE = NAIVE(cagr_10_year),
-  #       SNAIVE = SNAIVE(cagr_10_year),
-  #       NNETAR = NNETAR(cagr_10_year),
-  #       AR = AR(cagr_10_year),
-  #       VAR = VAR(cagr_10_year)) %>% 
-  # mutate(COMBINATION = (ARIMA + SNAIVE + VAR + NAIVE + RW) / 5,
-  #        ARIMA_SNAIVE = (ARIMA + SNAIVE) / 2)
+#       ETS = ETS(cagr_10_year),
+#       RW = RW(cagr_10_year),
+#       NAIVE = NAIVE(cagr_10_year),
+#       SNAIVE = SNAIVE(cagr_10_year),
+#       NNETAR = NNETAR(cagr_10_year),
+#       AR = AR(cagr_10_year),
+#       VAR = VAR(cagr_10_year)) %>% 
+# mutate(COMBINATION = (ARIMA + SNAIVE + VAR + NAIVE + RW) / 5,
+#        ARIMA_SNAIVE = (ARIMA + SNAIVE) / 2)
 
 # # Training set accuracy
 # models_ts %>% accuracy()
 # 
 # models_ts %>% print(n = 50)
 
+# Make forecasts and remove leakage
 fcast <- models_ts %>% 
   forecast(test)
 
-# Remove leakage
 fcast_no_leakage <- fcast %>% 
   filter(date > yearmonth(leakage_end_date))#,
-         # .model == "ARIMA")
+# .model == "ARIMA")
 
 # mean_fcast_no_leakage <- fcast %>% 
 #   filter(date > yearmonth("2005-01-01"),
 #          .model == "MEAN(cagr_10_year)")
 
-
+# Calculate leakage-free accuracies
 acc_no_leakage <- fcast_no_leakage %>% 
   accuracy(bind_rows(training, test)) %>% 
   select(.model, country, .type, RMSE, MAE, MAPE) %>% 
@@ -121,23 +137,7 @@ acc_no_leakage <- fcast_no_leakage %>%
   arrange(MAE) %>% 
   print(n = 100)
 
-# TODO
-# Accuracy of the model with additional features
-acc_no_leakage %>% 
-  filter(.model == "ARIMA") %>% 
-  inner_join(acc_no_leakage_arima %>% select(country)) %>% 
-  pull(MAE) %>% mean
-
-# Accuracy of the comparable ARIMA models
-acc_no_leakage_arima %>% 
-  inner_join(acc_no_leakage %>% 
-               filter(.model == "ARIMA") %>% 
-               select(country)) %>% 
-  pull(MAE) %>% 
-  mean(na.rm = TRUE)
-
-fcast_acc <- acc_no_leakage %>% arrange(MAE)
-
+# Plot all sets with forecasts
 fcast %>%
   filter(.model == "ARIMA") %>% 
   # filter(country == "AUSTRALIA") %>% 
@@ -151,8 +151,8 @@ fcast %>%
            xmin = as.Date(split_date), xmax = as.Date(leakage_end_date),
            ymin = -Inf, ymax = Inf) +
   scale_x_yearmonth(labels = year(seq.Date(fcast_start_date,
-                                                as.Date(max(test$date)),
-                                                by = "5 years")),
+                                           as.Date(max(test$date)),
+                                           by = "5 years")),
                     breaks = yearmonth(seq.Date(fcast_start_date,
                                                 as.Date(max(test$date)),
                                                 by = "5 years"))) +
@@ -161,21 +161,18 @@ fcast %>%
              color = "gray", linetype = "dashed") +
   geom_vline(xintercept = as.Date(leakage_end_date),
              color = "gray", linetype = "dashed") +
-  # scale_x_continuous() + 
   theme_minimal() +
   expand_limits(x = fcast_start_date) +
   theme(legend.position = c(0.95, 0),
         legend.justification = c(1, 0),
         axis.text.x = element_text(angle = 45))
 
-arima_acc <- fcast_acc %>% 
+# Calculate and compare accuracies of two different model types
+arima_acc <- acc_no_leakage %>% 
   filter(.model == "ARIMA")
 
-mean_fcast_acc <- fcast_acc %>% 
-  filter(.model == "MEAN")
-
-non_arima_acc <- mean_fcast_acc %>% 
-  filter(.model == "MEAN") %>% 
+non_arima_acc <- acc_no_leakage %>% 
+  filter(.model == model_to_compare) %>% 
   mutate(MAE_mean = MAE,
          MAPE_mean = MAPE) %>% 
   select(country, MAE_mean, MAPE_mean)
@@ -186,8 +183,7 @@ arima_acc %>%
          MAPE_diff = MAPE - MAPE_mean,
          MAPE_div = MAPE_mean / MAPE) %>% 
   arrange(-MAPE_div) %>% 
-  print(n = 50) #%>%
-   # pull(MAPE_div) %>% mean(na.rm = TRUE)
+  print(n = 50)
 
 # Average accuracy of every model
 acc_no_leakage %>% 
